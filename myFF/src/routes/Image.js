968f771b2483460e739f6cc6340c00e6afe4e994
ksojin1 <trueksj1121@naver.com>
2022-10-17 05:13:19 +0900

    이미지 view 진행중
    이미지 업로드 미리보기, 삭제, width 크기 조절

--- myFF/src/routes/Image.js
+++ myFF/src/routes/Image.js
@@ -1,26 +1,66 @@
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import Styles from "./Image.module.scss";
+import reactImageSize from "react-image-size";
 
-function UserMap() {
+function Image() {
+
+  const inputRef = useRef(null);
+  const imgRef = useRef(null);
+
+  const [image, setImage] = useState({
+    image_file: "",
+    preview_URL: "logo192.png",
+  });
+
+  const saveImage = (e) => {
+    e.preventDefault();
+    const fileReader = new FileReader();
+
+    if(e.target.files[0]){
+      fileReader.readAsDataURL(e.target.files[0]);
+    }
+    fileReader.onload = () => {
+      setImage({
+        image_file: e.target.files[0],
+        preview_URL: fileReader.result,
+      });
+    }
+  }
+
+  const deleteImage = () => {
+    setImage({
+      image_file: "",
+      preview_URL: "logo192.png",
+    });
+  }
+
+  const imgOnload = () => {
+
+    reactImageSize(image.preview_URL)
+      .then(({ width, height }) => {
+        imgRef.current.width = 300;
+      })
+      .catch((error) => console.log(error));
+  }
 
   return (
-    <div className="UserMap">
+    <div className="Image">
       <input type='file' accept="image/*"
         onChange={saveImage}
         onClick={(e) => e.target.value = null}
-        ref={refParam => inputRef = refParam}
+        ref={inputRef}
         style={{display: 'none'}}
       />
-
-      <div>
-        <img src={image.preview_URL}/>
+      <div className={Styles.imgDiv}>
+        <img src={image.preview_URL} ref={imgRef} onLoad={imgOnload} />
       </div>
-
       <div>
-        <button onClick={() => inputRef.click()}>Preview</button>
+        <button onClick={() => inputRef.current.click()}>Preview</button>
         <button onClick={deleteImage}>Delete</button>
-        <button onClick={sendImageToServer}>Upload</button>
       </div>
     </div>
   );
 }
 
-export default UserMap;
+export default Image;
