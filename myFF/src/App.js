6c2f8c54305755bd7526aba10eb4a078276c6471
ksojin1 <trueksj1121@naver.com>
2022-10-21 21:20:35 +0900

    useMemo Test

--- myFF/src/App.js
+++ myFF/src/App.js
@@ -22,7 +22,7 @@ function App() {
     <>
     <TestContext.Provider value={{user, setUser}}>
     <BrowserRouter>    
-      <Header />
+      {/* <Header /> */}
       <Routes>
 
         {/*test*/}
